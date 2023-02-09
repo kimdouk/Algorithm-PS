fae87f84ad47eeefb096b8bd093f7e7b2acbdbf9
kimdouk <89720795+kimdouk@users.noreply.github.com>
2023-02-09 20:00:22 +0900

    [Silver IV] Title: 수 찾기, Time: 424 ms, Memory: 47124 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,20 +1,18 @@
-def binary_search(array, target, start, end):
-    if start>end:
-        return None
-    mid = (start+end)//2
-    if array[mid] == target:
-        return mid
-    elif array[mid] > target:
-        return binary_search(array, target, start, mid-1)
-    else:
-        return binary_search(array, target, mid+1, end)
+def binary_search(array,target,start,end):
+    while start<=end:
+        mid = (start + end)//2
+        if array[mid] == target:
+            return 1
+        elif array[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return 0
 
-m = int(input())
-array = sorted(list(map(int,input().split())))
 n = int(input())
-test = list(map(int,input().split()))
+array = sorted(list(map(int,input().split())))
+m = int(input())
+data = list(map(int,input().split()))
 
-for i in range(n):
-    result = binary_search(array, test[i], 0, m-1)
-    if result == None:print(0)
-    else:print(1)
+for target in data:
+    print(binary_search(array,target,0,n-1))
