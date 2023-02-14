77f13db8fb0545cf6f3749ed2a19506bb217c183
kimdouk <89720795+kimdouk@users.noreply.github.com>
2023-02-14 21:23:38 +0900

    [Silver III] Title: N과 M (5), Time: 196 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,13 +1,16 @@
-n, m = map(int,input().split())
+n,m=map(int,input().split())
+data = [0]+sorted(list(map(int,input().split())))
 result = []
-data = sorted(list(map(int,input().split())))
+visited = [False] * (max(data)+1)
 def dfs():
     if len(result) ==m:
         print(*result)
         return
-    for i in data:
-        if i not in result:
-            result.append(i)
+    for i in range(1,n+1):
+        if not visited[data[i]]:
+            visited[data[i]] = True
+            result.append(data[i])
             dfs()
             result.pop()
+            visited[data[i]] = False
 dfs()
