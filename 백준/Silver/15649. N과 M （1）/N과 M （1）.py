0442d79c71cbc1f8c7d276d135007890ece1617a
kimdouk <89720795+kimdouk@users.noreply.github.com>
2023-02-14 20:17:24 +0900

    [Silver III] Title: N과 M (1), Time: 188 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,17 +1,15 @@
 n, m = map(int,input().split())
-visit = [0]*(n+1)
-array = []
-
-def dfs(step):
-    if step == m:
-        # print(' '.join(map(str,array)))
-        print(*array)
+result = []
+visited = [False] * (n+1)
+def dfs():
+    if len(result) == m:
+        print(*result)
         return
     for i in range(1,n+1):
-        if not visit[i]:
-            visit[i] = 1
-            array.append(i)
-            dfs(step+1)
-            visit[i] = 0
-            array.pop()
-dfs(0)
+        if not visited[i]:
+            visited[i] = True
+            result.append(i)
+            dfs()
+            result.pop()
+            visited[i] = False
+dfs()
