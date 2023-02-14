a9f3462f4693afb8d2f565603a0c75807921e387
kimdouk <89720795+kimdouk@users.noreply.github.com>
2023-02-14 20:57:27 +0900

    [Silver III] Title: N과 M (2), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,15 +1,6 @@
-n, m = map(int,input().split())
-result = []
-visited = [False] * (n+1)
-def dfs(idx):
-    if len(result) ==m:
-        print(*result)
-        return
-    for i in range(idx,n+1):
-        if not visited[i]:
-            visited[i] = True
-            result.append(i)
-            dfs(i+1)
-            result.pop()
-            visited[i] = False
-dfs(1)
+from itertools import combinations
+n, m =map(int,input().split())
+data = [i for i in range(1,n+1)]
+result = combinations(data,m)
+for i in result:
+    print(*i)
