f967858de66b65d287d5b8e7452b909003b79ecd
kimdouk <89720795+kimdouk@users.noreply.github.com>
2023-02-14 21:24:47 +0900

    [Silver III] Title: N과 M (5), Time: 184 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,16 +1,6 @@
+from itertools import permutations
 n,m=map(int,input().split())
-data = [0]+sorted(list(map(int,input().split())))
-result = []
-visited = [False] * (max(data)+1)
-def dfs():
-    if len(result) ==m:
-        print(*result)
-        return
-    for i in range(1,n+1):
-        if not visited[data[i]]:
-            visited[data[i]] = True
-            result.append(data[i])
-            dfs()
-            result.pop()
-            visited[data[i]] = False
-dfs()
+data = sorted(list(map(int,input().split())))
+result = permutations(data,m)
+for i in result:
+    print(*i)
