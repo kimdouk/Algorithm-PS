ea143cc07a438a704ad6852b8bb1def0ae1b6912
kimdouk <89720795+kimdouk@users.noreply.github.com>
2023-02-17 21:27:40 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 516 ms, Memory: 115880 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,22 +1,26 @@
-import sys
-sys.setrecursionlimit(10**4)
+def find_parent(parent,x):
+    if parent[x] !=x:
+        parent[x] = find_parent(parent,parent[x])
+    return parent[x]
 
-def dfs(v):
-    visited[v] = True
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(i)
+def union_parent(parent,a,b):
+    a = find_parent(parent,a)
+    b = find_parent(parent,b)
+    if a<b:
+        parent[b] = a
+    else:
+        parent[a] = b
 
 n,m = map(int,input().split())
-visited = [False] *(n+1)
-graph = [[] for _ in range(n+1)]
+parent = [0] * (n+1)
+for i in range(1,n+1):
+    parent[i] = i
+
+result = []
 for i in range(m):
     a,b = map(int,input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-result = 0
+    union_parent(parent,a,b)
 for i in range(1,n+1):
-    if not visited[i]:
-        dfs(i)
-        result+=1
-print(result)
+    result.append(find_parent(parent,i))
+
+print(len(set(result)))
