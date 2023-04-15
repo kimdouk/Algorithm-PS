c9127e63e4a6d65623b959930ae455ce3d28e288
kimdouk <89720795+kimdouk@users.noreply.github.com>
2023-04-15 23:59:22 +0900

    [Bronze III] Title: 공 넣기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,7 @@
+n,m = map(int,input().split())
+basket = [0]*(n+1)
+for _ in range(m):
+    i,j,k = map(int,input().split())
+    for idx in range(i,j+1):
+        basket[idx] = k
+print(*basket[1:])
