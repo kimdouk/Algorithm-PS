dc8de317dfe69c6655924835f6aa859cad33ee9e
Douk Kim <89720795+kimdouk@users.noreply.github.com>
2023-06-04 16:29:44 +0900

    [Silver II] Title: 이항 계수 2, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -1,6 +1,7 @@
 n,k = map(int,input().split())
-dp = [[1 for _ in range(n+1)] for _ in range(n+1)]
-for i in range(2,n+1):
-    for j in range(1,i):
-        dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%10007
-print(dp[n][k])
+result = 1
+for i in range(n,n-k,-1):
+    result*=i
+for i in range(k,0,-1):
+    result//=i
+print(result%10007)
