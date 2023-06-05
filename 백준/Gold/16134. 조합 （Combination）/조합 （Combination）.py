03c0a4f012e851defdb5781d83ec947ea61f5f00
Douk Kim <89720795+kimdouk@users.noreply.github.com>
2023-06-05 22:45:14 +0900

    [Gold I] Title: 조합 (Combination), Time: 368 ms, Memory: 71248 KB -BaekjoonHub

---
+++ 백준/Gold/16134. 조합 （Combination）/조합 （Combination）.py
@@ -0,0 +1,17 @@
+def power(a,b):
+    if b==0:
+        return 1
+    if b%2:
+        return ((power(a,b//2) **2) *a)%p
+    else: 
+        return (power(a,b//2) **2)%p
+
+p = 1000000007
+fact = [1 for _ in range(1000001)]
+for i in range(1,1000001):
+    fact[i] = (fact[i-1]*i)%p
+
+n,r = map(int,input().split())
+a = fact[n]
+b = (fact[n-r]*fact[r])%p
+print(((a%p)*(power(b,p-2)%p))%p)
